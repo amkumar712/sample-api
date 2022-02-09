da48e5bc023316edc2415e3d57b4c5cac7888a2e
Muthu <amkumar712@gmail.com>
2022-02-08 19:08:03 -0500

    Update with application properties

--- src/main/java/org/studio/sample/controller/SampleController.java
+++ src/main/java/org/studio/sample/controller/SampleController.java
@@ -1,6 +1,7 @@
 package org.studio.sample.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class SampleController {
     @GetMapping("/")
     public String getOK() {
         return "All Good.";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(@RequestHeader("name") String name) {
+        return "Welcome " + name;
     }
 }
